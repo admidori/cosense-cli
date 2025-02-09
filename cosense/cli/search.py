9ded517f42493650db797a77779ec8e0b9b91a0d
Aoi Kondo <agotadmidori@gmail.com>
2025-02-09 22:12:25 +0900

    feat: Update functions about reading an article

--- cosense/cli/search.py
+++ cosense/cli/search.py
@@ -1,5 +1,6 @@
 import curses
 import sys
+from turtle import st
 import cosense
 from functools import partial
 import locale
@@ -8,6 +9,7 @@ import datetime
 import threading
 import math
 import time
+import json
 
 def search(args):
     locale.setlocale(locale.LC_ALL, "")
@@ -20,7 +22,9 @@ def read_page(page, text, stdscr):
     created = created_date.strftime("%Y-%m-%d")
     updated_date = datetime.datetime.fromtimestamp(int(page["updated"]))
     updated = updated_date.strftime("%Y-%m-%d")
-    
+
+    text = str(text)
+    start_num = 0
     while True:
         height, width = stdscr.getmaxyx()
         stdscr.addstr(0, 0, "quit: 'q' / move page: 'j' or 'k'", curses.A_REVERSE)
@@ -30,12 +34,20 @@ def read_page(page, text, stdscr):
         stdscr.addstr(3, 0, f"Updated: {updated}")
         stdscr.addstr(4, 0, f"Views: {page['views']}")
         stdscr.addstr(5, 0, "=" * 50)
-        stdscr.addstr(6, 0, text)
+
+        appear_text = text.split("\n")[start_num:start_num + height - 7]
+        stdscr.addstr(6, 0, "\n".join(appear_text))
 
         key = stdscr.getkey()
 
         if key == "q":
             break
+        elif key == "k":
+            start_num = max(0, start_num - (height - 7))
+            stdscr.clear()
+        elif key == "j":
+            start_num = min(len(text.split("\n")), start_num + (height - 7))
+            stdscr.clear()
         else:
             stdscr.addstr(0, 0, f"Unknown key: {key}", curses.A_REVERSE)
 
@@ -85,6 +97,7 @@ def select_pages(stdscr, args):
     texts = []
     page_number = 0
     selected = 1
+    IS_FETCHED = False
 
     # Initialize client
     if args.auth is None:
@@ -117,9 +130,8 @@ def select_pages(stdscr, args):
         page_total = math.ceil((page_total_pj * 100) / DISPLAY_PAGES)
 
         # Menubar
-        with lock:
-            len_pages = len(pages)
-            len_texts = len(texts)
+        len_pages = len(pages)
+        len_texts = len(texts)
 
         download_status = ""
         if len_pages < page_total:
@@ -127,6 +139,7 @@ def select_pages(stdscr, args):
         elif len_texts < len_pages:
             download_status = "Donwloading texts: " + "-"*int((len_texts + 1) / 10) + " "*int((len_pages / 10) - ((len_texts + 1) / 10))
         else:
+            IS_FETCHED = True
             download_status = "'J' to output the project to json file"
         stdscr.addstr(height - 2, 0, f"Project: {args.project} Page: {page_number+1}/{page_total} {download_status}", curses.A_REVERSE)
         stdscr.addstr(height - 1, 0, "quit: 'q' / read: 'Enter' / select article: 'j' or 'k' / move page: 'h' or 'l'", curses.A_REVERSE)
@@ -150,9 +163,40 @@ def select_pages(stdscr, args):
             page_number = min(page_total - 1, page_number + 1)
             selected = 1
         elif key == "\n":
-            pagenum = 45 * page_number + selected - 1
-            read_page(pages[selected - 1], texts[pagenum], stdscr)
+            if IS_FETCHED:
+                pagenum = DISPLAY_PAGES * page_number + selected - 1
+                read_page(pages[selected - 1], texts[pagenum], stdscr)
+                stdscr.clear()
         elif key == "q":
+            sys.exit(0)
+        elif key == "J":
+            with open("output.json", "w") as f:
+                output_json = {
+                    "name": "",
+                    "displayName":"",
+                    "exported":"",
+                    "users":[{
+                        "id":"",
+                        "name":"",
+                        "displayName":"",
+                        "email":""
+                    }],
+                    "pages": []
+                }
+                for page in pages:
+                    t = str(texts[pages.index(page)])
+                    new_text = t.split("\n")
+
+                    new_page = {
+                        "title": page["title"],
+                        "created": page["created"],
+                        "updated": page["updated"],
+                        "id": page["id"],
+                        "views": page["views"],
+                        "lines": new_text
+                    }
+                    output_json["pages"].append(new_page)
+                f.write(json.dumps(output_json, indent=2, ensure_ascii=False))
             sys.exit(0)
         else:
             stdscr.addstr(0, 0, f"Unknown key: {key}", curses.A_REVERSE)
