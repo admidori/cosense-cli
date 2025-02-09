39ecaf91bfc2057099c46e50894d357bf89e46c9
Aoi Kondo <agotadmidori@gmail.com>
2025-02-09 13:30:47 +0900

    feat: Update init files

---
+++ cosense/__init__.py
@@ -0,0 +1,5 @@
+from .lib import Client
+__all__ = ['Client']
+__version__ = '0.1.0'
+
+USER_AGENT = 'Cosense Python API Wrapper %s' % __version__
--- scrapbox/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.6.1'
-__all__ = ['Client']
-
-USER_AGENT = 'Scrapbox Python API Wrapper %s' % __version__
-
-from scrapbox.client import Client
